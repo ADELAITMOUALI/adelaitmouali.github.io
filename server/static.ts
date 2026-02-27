b341dfef0712439f4ce6086cfc78a211a10e9091
moualidavid <moualidavid@gmail.com>
2026-02-27 02:50:15 +0000

    Fix: __dirname is not defined in ES module scope

--- server/static.ts
+++ server/static.ts
@@ -1,4 +1,8 @@
 import { static as expressStatic } from "express";
-import { join } from "path";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const staticRouter = expressStatic(join(__dirname, "..", "public"));
